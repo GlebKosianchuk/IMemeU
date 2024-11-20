ea6f80244220e0abe9c3eec1d98369d012e5b983
GlebKosianchuk <83186808+GlebKosianchuk@users.noreply.github.com>
2024-11-20 19:40:03 +0300

    Revert changes

--- IMemeU/Migrations/AppDbContextModelSnapshot.cs
+++ IMemeU/Migrations/AppDbContextModelSnapshot.cs
@@ -21,7 +21,7 @@ namespace Messager.Migrations
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
 
-            modelBuilder.Entity("IMemeU.Data.User", b =>
+            modelBuilder.Entity("Messager.Data.User", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
